201c20f51cbdc0ca9f5833488f7b233eb196e595
Ilya Shishkov <shishkovilja@yandex.ru>
2019-11-15 22:56:42 +0300

    Added Apache Ignite continuous cache filling (gargantua)

--- src/main/java/ru/shishkov/IgniteMemoryOvercommitTest.java
+++ src/main/java/ru/shishkov/IgniteMemoryOvercommitTest.java
@@ -1,7 +1,43 @@
 package ru.shishkov;
 
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.IgniteDataStreamer;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.configuration.DataStorageConfiguration;
+import org.apache.ignite.configuration.IgniteConfiguration;
+
 public class IgniteMemoryOvercommitTest {
     public static void main(String[] args) {
+        Ignite ignite = startIgnite();
 
+        startEagerEating(ignite);
+    }
+
+    private static void startEagerEating(Ignite ignite) {
+        String cacheName = "stomach";
+        int entitySz = 3800;
+
+        IgniteCache<Long, byte[]> cache = ignite.getOrCreateCache(cacheName);
+
+        try (IgniteDataStreamer<Long, byte[]> streamer = ignite.dataStreamer(cacheName)) {
+            for (long l = 0; l < Long.MAX_VALUE; l++) {
+                streamer.addData(l, new byte[entitySz]);
+            }
+        }
+    }
+
+    private static Ignite startIgnite() {
+        long freeMem = Runtime.getRuntime().freeMemory();
+
+        DataStorageConfiguration dsCfg = new DataStorageConfiguration();
+        dsCfg.getDefaultDataRegionConfiguration().setMaxSize(freeMem);
+        IgniteConfiguration cfg = new IgniteConfiguration().setDataStorageConfiguration(dsCfg);
+
+        return Ignition.start(cfg);
+    }
+
+    private static long getFreeMemory() {
+        return Runtime.getRuntime().freeMemory();
     }
 }
