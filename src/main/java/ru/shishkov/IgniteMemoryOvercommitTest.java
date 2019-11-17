051d97bb728be6da77b2c83cc5f53b9deb443ea6
Ilya Shishkov <shishkovilja@yandex.ru>
2019-11-17 23:58:57 +0300

    Added determining of free memory size, added manifest and jar-packaging to pom.xml

--- src/main/java/ru/shishkov/IgniteMemoryOvercommitTest.java
+++ src/main/java/ru/shishkov/IgniteMemoryOvercommitTest.java
@@ -1,11 +1,14 @@
 package ru.shishkov;
 
+import com.sun.management.OperatingSystemMXBean;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.IgniteDataStreamer;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.configuration.DataStorageConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
+
+import java.lang.management.ManagementFactory;
 
 public class IgniteMemoryOvercommitTest {
     public static void main(String[] args) {
@@ -28,16 +31,13 @@ public class IgniteMemoryOvercommitTest {
     }
 
     private static Ignite startIgnite() {
-        long freeMem = Runtime.getRuntime().freeMemory();
+        OperatingSystemMXBean osMXBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+        long freeMem = osMXBean.getFreePhysicalMemorySize() * 2;
 
         DataStorageConfiguration dsCfg = new DataStorageConfiguration();
         dsCfg.getDefaultDataRegionConfiguration().setMaxSize(freeMem);
         IgniteConfiguration cfg = new IgniteConfiguration().setDataStorageConfiguration(dsCfg);
 
         return Ignition.start(cfg);
-    }
-
-    private static long getFreeMemory() {
-        return Runtime.getRuntime().freeMemory();
     }
 }
